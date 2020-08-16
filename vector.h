169ce63cc84467dcb12443692a6dff6a382b05f0
tonyp7 <bubbathemaster@gmail.com>
2020-08-16 15:48:18 +0800

    documentation is starting to come live

--- vector.h
+++ vector.h
@@ -46,13 +46,45 @@ typedef struct vector_t {
 
 #define VECTOR_DEFAULT_INITIAL_SIZE 2
 
+/**
+  * @brief create an empty vector with an initial capacity of VECTOR_DEFAULT_INITIAL_SIZE
+  * @param		size_type: size in bytes of the elements to be stored
+  * @return		vector_t*: success
+  *				NULL: failure
+  */
+vector_t* vector_create(size_t size_type);
 
-vector_t* vector_create(size_t size_type, size_t capacity);
+/**
+  * @brief create an empty vector with an initial capacity of VECTOR_DEFAULT_INITIAL_SIZE
+  * @param		size_type: size in bytes of the elements to be stored
+  * @param		capacity: initial capacity in number of elements that will be allocated
+  * @return		vector_t*: success
+  *				NULL: failure
+  */
+vector_t* vector_create_with(size_t size_type, size_t capacity);
 
+/**
+  * @brief free all memory allocated to the vector and its elements.
+  * @param  vector: the vector to perform the operation on
+  * @note vector's value will be set to NULL after being freed
+  */
 void vector_free(vector_t** vector);
 
-void* vector_at(vector_t* vector, int n);
-
+/**
+ * @brief sort the given vector according to the comparator passed as argument
+ * @param  vector: the vector to perform the operation on
+ * @param  comp: a standard comparator function
+ * @return 0: success
+ *         -1: failure
+ * @code{c}
+ * // A typical comparator for integers would be defined as such:
+ * int int_comparator(const void* a, const void* b)
+ * {
+ *    return *((int*)a) - *((int*)b);
+ * }
+ * @endcode
+ * @note internally, vector_sort will call stdlib's quick sort implementation
+ */
 int vector_sort(vector_t* vector, int (*comp)(const void*, const void*));
 
 int vector_push_back(vector_t* vector, const void* data);
@@ -69,9 +101,32 @@ int vector_pop_front(vector_t* vector, void* data);
 
 int vector_erase(vector_t* vector, int n);
 
+int vector_shrink_to_fit(vector_t* vector);
+
+/**
+  * @brief access the vector's first value
+  * @param		vector: the vector to perform the operation on
+  * @return		void*: pointer to the data
+  *				NULL: failure
+  */
 void* vector_front(vector_t* vector);
 
+/**
+  * @brief access the vector's last value
+  * @param		vector: the vector to perform the operation on
+  * @return		void*: pointer to the data
+  *				NULL: failure
+  */
 void* vector_back(vector_t* vector);
+
+/**
+  * @brief access the vector's nth value
+  * @param		vector: the vector to perform the operation on
+  * @param		n: the 0 indexed n th value
+  * @return		void*: pointer to the data
+  *				NULL: failure
+  */
+void* vector_at(vector_t* vector, int n);
 
 
 
